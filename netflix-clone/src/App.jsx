9ebb19a45da99d540f4b9365561c9c286ea339a9
Merisola <meronsolomondana@gmail.com>
2025-09-12 20:27:40 +0300

    add rowlist

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import Home from './pages/Home/Home'
+import Home from './pages/Home/index'
 
 function App() {
 
--- netflix-clone/src/Components/Rows/RowList/RowList.jsx
+++ netflix-clone/src/Components/Rows/RowList/RowList.jsx
@@ -1,24 +1,47 @@
-import React from 'react'
+// RowList.jsx
+import React from "react";
 import Row from "../Row/Row";
-import requests from '../../../utils/requests';
+import requests from "../../../utils/requests";
 
 const RowList = () => {
+  // Helper to format keys into readable titles
+  const formatTitle = (key) => {
+    switch (key) {
+      case "fetchNetflixOriginals":
+        return "Netflix Originals";
+      case "fetchTrending":
+        return "Trending";
+      case "fetchTopRated":
+        return "Top Rated";
+      case "fetchActionMovies":
+        return "Action Movies";
+      case "fetchComedyMovies":
+        return "Comedy Movies";
+      case "fetchHorrorMovies":
+        return "Horror Movies";
+      case "fetchRomanceMovies":
+        return "Romance Movies";
+      case "fetchDocumentaries":
+        return "Documentaries";
+      case "fetchPopularTV":
+        return "Popular TV Shows";
+      default:
+        return "Movies";
+    }
+  };
+
   return (
     <div>
-      <Row
-        title="NETFLIX ORIGINALS"
-        fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow={true}
-      />
-      <Row
-        title="Trending"
-        fetchUrl={requests.fetchTrending}
-        // isLargeRow={true}
-      />
-      <Row />
-      <Row />
+      {Object.entries(requests).map(([key, url]) => (
+        <Row
+          key={key}
+          title={formatTitle(key)}
+          fetchUrl={url}
+          isLargeRow={key === "fetchNetflixOriginals"} 
+        />
+      ))}
     </div>
   );
-}
+};
 
 export default RowList;
